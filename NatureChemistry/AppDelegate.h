f5c348ed9adbd764e676cdd9e3f3c1637efe6d30
Huy Truong <huytcm.1708@gmail.com>
2016-04-05 09:52:17 +0700

    Initial Commit

---
+++ NatureChemistry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NatureChemistry
+//
+//  Created by HuyTCM on 4/5/16.
+//  Copyright © 2016 HuyTCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NatureChemistry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NatureChemistry
+//
+//  Created by HuyTCM on 4/5/16.
+//  Copyright © 2016 HuyTCM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
